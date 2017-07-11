3eb983c847084510bdac0467ef531aeaf6dd7821
htwright <harrisontwright@gmail.com>
2017-07-11 16:49:58 -0500

    cleaned up

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 filename = './sample.txt'
 import codecs
+import operator
+import random
 
 with open(filename, 'r') as f:
-  # f.decode('cp1252')
   content = f.read(15000)
-  # print(content)
-# print(content)
-
-# print(type(content))
 
 
 def filter_str(str1, str2):
@@ -15,13 +12,9 @@ def filter_str(str1, str2):
     str1=str1.replace(c,'')
   return str1
 
-# content.translate('.@[]{}()\'\"/\\')
 filters = '.@[]{}()\'\"/\\'
 content = filter_str(content, filters)
 lst = content.split(' ')
-# print(lst)
-
-
 values = {}
 
 for str in lst:
@@ -30,27 +23,14 @@ for str in lst:
   else:
     values[str] += 1
 
-
-# print(values)
-# with codecs.open(filename, 'a', 'cp1252') as f:
-  # print(f)
-  # data = f.read(1)
-  # print(data)
-
-# sorted(values.iterkeys(), key=lambda k: values[k], reverse = True)
-
-import operator
 sorted_values = sorted(values.items(), key=operator.itemgetter(1), reverse=True)
 
-# print(sorted_values)
-import random
 def generate_936(tuples):
   result = []
   for i in range(4):
     j = random.randint(0, 20)
     x, _ = tuples[j]
     result.append(x)
-  # print(result)
   return '%s - %s - %s - %s' % (result[0], result[1], result[2], result[3])
 testvar = generate_936(sorted_values)
 print(testvar)
