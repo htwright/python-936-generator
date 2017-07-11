a44fe27cbf1feb8ff84dc825e6abbe40ee4fcbbe
htwright <harrisontwright@gmail.com>
2017-07-11 17:51:44 -0500

    increased sample size, fixed memory overflow

--- app.py
+++ app.py
@@ -2,21 +2,18 @@ import codecs
 import operator
 import random
 filename = './sample1.txt'
-#current sample file includes 6 text files from wikipedia text dump compiled into a single utf-8.txt in order to avoid errors.
-#~31 million characters pre filter, ~31MB
+#current sample file includes 13 text files from wikipedia text dump compiled into a single utf-8.txt in order to avoid errors.
+#~75 million characters pre filter, ~74MB
 with open(filename, 'r', encoding='utf-8') as f:
   content = f.read()
-
+  print('Generating 936 password with %s character sample size. Please wait...' % (len(content)))
 
 def filter_str(str1, str2):
   for c in str2:
     str1=str1.replace(c,'')
   return str1
-print(len(content))
 filters = '.@[]{}()\'\"/\\-=_+;,:!#$%^&*'
-content = content.lower()
 content = filter_str(content, filters)
-print(len(content))
 lst = content.split(' ')
 values = {}
 lst1 = []
@@ -25,10 +22,10 @@ for str in lst:
     lst1.append(str)
 
 for str in lst1:
-  if str not in values:
-    values[str] = 1;
+  if str.lower() not in values:
+    values[str.lower()] = 1;
   else:
-    values[str] += 1
+    values[str.lower()] += 1
 #returns sorted (descending) tuples
 sorted_values = sorted(values.items(), key=operator.itemgetter(1), reverse=True)
 
@@ -40,5 +37,6 @@ def generate_936(tuples):
     result.append(x)
   return '%s - %s - %s - %s' % (result[0], result[1], result[2], result[3])
 testvar = generate_936(sorted_values)
-print(testvar)
+print('936 password - ', testvar)
+print('Length of filtered, sorted list of words')
 print(len(sorted_values))
