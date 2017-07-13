a997fcb0144af828f3703e74344296f2d306746c
htwright <harrisontwright@gmail.com>
2017-07-13 09:39:36 -0500

    cleaned up

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import codecs
 import operator
 import random
 from collections import Counter
+import tkinter as tk
 filename = './sample.txt'
 #current sample.txt file includes 2 text files from wikipedia text dump compiled into a single utf-8.txt in order to avoid errors.
 #~6.5 million characters pre filter, ~7MB
@@ -29,45 +30,22 @@ for str in lst:
     lst1.append(str.lower())
 
 c = Counter(lst1)
-# for str in lst1:
-#   if str.lower() not in values:
-#     values[str.lower()] = 1;
-#   else:
-#     values[str.lower()] += 1
-# #returns sorted (descending) tuples. The program hangs here.
-# #TODO: Improve runtime of this step.
-# sorted_values = sorted(values.items(), key=operator.itemgetter(1), reverse=True)
 sorted_values = c.most_common(5000);
+
 def generate_936(tuples = sorted_values):
   result = []
   for i in range(4):
-    j = random.randint(0, 5000)
+    j = random.randint(0, 4999)
     x, _ = tuples[j]
     result.append(x)
   return '%s - %s - %s - %s' % (result[0], result[1], result[2], result[3])
+
+
 passwords = []
 for i in range(10):
   passwords.append(generate_936(sorted_values))
-print('Length of filtered, sorted list of words')
-print(len(sorted_values))
-# for i in range(1900,2000):
-  # print(sorted_values[i])
 password = generate_936(sorted_values)
-import tkinter as Tk
-import tkinter.tix as Tix
 
-# root = Tix.Tk()
-# setup HList
-# hl = Tix.HList(root, columns = 5, header = True)
-# hl.header_create(0, text = "File")
-# hl.header_create(1, text = "Date")
-# hl.header_create(1, text = "Size")
-# create a multi-column row
-# hl.add("row1", text = password)
-# hl.item_create(entry_path, 1, text = "2009-03-26 21:07:03")
-# hl.item_create(entry_path, 2, text = "200MiB")
-
-import tkinter as tk
 
 class Application(tk.Frame):
     def __init__(self, master=None):
